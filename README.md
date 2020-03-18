7865ac87581a5b54b0a9e84ef3a915fa53096083
SebGogo <sgogola033108@gmail.com>
2020-03-18 09:36:33 -0400

    2 files modified, 3 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-fpa3y7
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fpa3y7)
--- main.js
+++ main.js
@@ -1,6 +1,56 @@
-// Import stylesheets
-import './style.css';
+// Cloud API base URL and header variables
+let url = 'https://api.accusoft.com/prizmdoc';
+let headers = new Headers();
+headers.append("Content-Type", "application/json");
+headers.append("Acs-Api-Key", "U7JHrsbzGGhn06aVFLm-PCBAVCGD1zJlkFNkI6EmjkOAzRNYPh101Vd5nnsnIh_x");
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// Get viewing session ID
+const getSessionId = () => {
+    let raw = JSON.stringify({ "source": { "type": "upload", "displayName": "someuniqueName" } });
+    let requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: raw,
+        redirect: 'follow'
+    };
+    fetch(`${url}/ViewingSession`, requestOptions)
+        .then(response => response.text())
+        .then(result => document.getElementById('sessionResponse').innerHTML = result);
+};
+document.getElementById('btnFetch').addEventListener('click', getSessionId);
+
+// Upload document with viewingSessionID
+const uploadDocument = () => {
+    let file = document.getElementById('fileUpload');
+    let requestOptions = {
+        method: 'PUT',
+        headers: { "Acs-Api-Key": "U7JHrsbzGGhn06aVFLm-PCBAVCGD1zJlkFNkI6EmjkOAzRNYPh101Vd5nnsnIh_x" },
+        body: file.files[0],
+        redirect: 'follow'
+    };
+
+    fetch(`${url}/ViewingSession/${getSessionId}/SourceFile`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+document.getElementById('fileUpload').addEventListener('change', uploadDocument);
+
+// Instantiate the viewer
+$(function () {
+    $('#viewerContainer').pccViewer({
+        documentID: getSessionId,
+        imageHandlerUrl: '/pas-proxy',
+        viewerAssetsPath: 'viewer-assets',
+        resourcePath: 'viewer-assets/img',
+        language: viewerCustomizations.languages['en-US'],
+        template: viewerCustomizations.template,
+        icons: viewerCustomizations.icons,
+        annotationsMode: "LayeredAnnotations"
+    });
+});
+
+
+
+
+
